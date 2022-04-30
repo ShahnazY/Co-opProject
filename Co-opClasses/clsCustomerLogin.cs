329196ffbb3798aa89d6e0ef1681aafd24c379fd
Shahnaz <P2574119@my365.dmu.ac.uk>
2022-04-30 17:49:05 +0100

    created find methods for customerlogin

--- Co-opClasses/clsCustomerLogin.cs
+++ Co-opClasses/clsCustomerLogin.cs
@@ -1,10 +1,65 @@
-﻿namespace Co_opClasses
+﻿using System;
+
+namespace Co_opClasses
 {
     public class clsCustomerLogin
     {
-        public int CustomerLoginID { get; set; }
-        public int CustomerID { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        private Int32 mCustomerLoginID;
+        private Int32 mCustomerID;
+        private string mEmail;
+        private string mPassword;
+        public int CustomerLoginID
+        {
+            get
+            {
+                return mCustomerLoginID;
+            }
+            set
+            {
+                mCustomerLoginID = value;
+            }
+        }
+        public int CustomerID
+        {
+            get
+            {
+                return mCustomerID;
+            }
+            set
+            {
+                mCustomerID = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return mEmail;
+            }
+            set
+            {
+                mEmail = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+
+        public bool Find(int CustomerLoginID)
+        {
+            mCustomerLoginID = 2;
+            mCustomerID = 5;
+            mEmail = "Mark.12@gmail.com";
+            mPassword = "password";
+            return true;
+        }
     }
 }
