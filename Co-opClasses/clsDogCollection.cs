d940ff81de425271762339fa5353eec8352604b8
ElizaP2410479 <P2410479@my365.dmu.ac.uk>
2022-05-02 20:47:49 +0100

    Added testing for clsDogCollection

--- Co-opClasses/clsDogCollection.cs
+++ Co-opClasses/clsDogCollection.cs
@@ -8,6 +8,23 @@ namespace Co_opClasses
 {
     public class clsDogCollection
     {
+        //private data member for the list 
+        List<clsDog> mDogList = new List<clsDog>();
+        //private data member thisdog
+        clsDog mThisDog = new clsDog();
+
+        public clsDog ThisDog
+        {
+            get
+            {
+                return mThisDog;
+            }
+            set
+            {
+                mThisDog = value;
+            }
+        }
+
         //constructor for the class
         public clsDogCollection()
         {
@@ -40,9 +57,6 @@ namespace Co_opClasses
                 Index++;
             }
         }
-
-
-        List<clsDog> mDogList = new List<clsDog>();
         public List<clsDog> DogsList
         {
             get
@@ -65,6 +79,65 @@ namespace Co_opClasses
 
             }
         }
-        public clsDog ThisDog { get; set; }
+            public int Add()
+            {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@Age", mThisDog.Age);
+            DB.AddParameter("@Breed", mThisDog.Breed);
+            DB.AddParameter("@Gender", mThisDog.Gender);
+            DB.AddParameter("@Name", mThisDog.Name);
+            DB.AddParameter("@Size", mThisDog.Size);
+            DB.AddParameter("@DateAdded", mThisDog.DateAdded);
+            //execute the query returning the primary key value
+            return DB.Execute("sproc_tblDog_Insert");
+            }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@DogID", mThisDog.DogID);
+            DB.Execute("sproc_tblDog_Delete");
+        }
+
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@DogID", mThisDog.DogID);
+            DB.AddParameter("@Age", mThisDog.Age);
+            DB.AddParameter("@Breed", mThisDog.Breed);
+            DB.AddParameter("@Gender", mThisDog.Gender);
+            DB.AddParameter("@Name", mThisDog.Name);
+            DB.AddParameter("@Size", mThisDog.Size);
+            DB.AddParameter("@DateAdded", mThisDog.DateAdded);
+            //execute the stored procedure
+            DB.Execute("sproc_tblDog_Update");
+        }
+
+        public void ReportByName(string Name)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@Name", Name);
+            DB.Execute("sproc_tblDog_FilterByName");
+            PopulateArray(DB);
+        }
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 RecordCount;
+            RecordCount = DB.Count;
+            mDogList = new List<clsDog>();
+            while (Index < RecordCount)
+            {
+                clsDog ADog = new clsDog();
+                ADog.Age = Convert.ToInt32(DB.DataTable.Rows[Index]["Age"]);
+                ADog.Breed = Convert.ToString(DB.DataTable.Rows[Index]["Breed"]);
+                ADog.Gender = Convert.ToString(DB.DataTable.Rows[Index]["Gender"]);
+                ADog.Name = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+                ADog.Size = Convert.ToString(DB.DataTable.Rows[Index]["Size"]);
+                ADog.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+                mDogList.Add(ADog);
+                Index++;
+            }
+        }
     }
 }
--- InsuranceTesting/tstDogCollection.cs
+++ InsuranceTesting/tstDogCollection.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using ClassLibrary;
 
 namespace DogTesting
 {
@@ -86,6 +87,105 @@ namespace DogTesting
             AllDogs.DogsList = TestList;
             //test to see it exists
             Assert.AreEqual(AllDogs.Count, TestList.Count);
+        }
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class
+            clsDogCollection AllDogs = new clsDogCollection();
+            //create the item of test data
+            clsDog TestItem = new clsDog();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.DogID = 2;
+            TestItem.Age = 2;
+            TestItem.Breed = "Bulldog";
+            TestItem.Gender = "Male";
+            TestItem.Name = "Rex";
+            TestItem.Size = "Small";
+            TestItem.DateAdded = DateTime.Now.Date;
+            //set Thisdog to the test data;
+            AllDogs.ThisDog = TestItem;
+            //add the record
+            PrimaryKey = AllDogs.Add();
+            //set the primary key of the test data
+            TestItem.DogID = PrimaryKey;
+            //find the record
+            AllDogs.ThisDog.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllDogs.ThisDog, TestItem);
+        }
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class
+            clsDogCollection AllDogs = new clsDogCollection();
+            //create the item of test data
+            clsDog TestItem = new clsDog();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.DogID = 2;
+            TestItem.Age = 2;
+            TestItem.Breed = "Bulldog";
+            TestItem.Gender = "Male";
+            TestItem.Name = "Rex";
+            TestItem.Size = "Small";
+            TestItem.DateAdded = DateTime.Now.Date;
+            //set Thisdog to the test data;
+            AllDogs.ThisDog = TestItem;
+            //add the record
+            PrimaryKey = AllDogs.Add();
+            //set the primary key of the test data
+            TestItem.DogID = PrimaryKey;
+            //find the record
+            AllDogs.ThisDog.Find(PrimaryKey);
+            //delete the record
+            AllDogs.Delete();
+            //now find the record
+            Boolean Found = AllDogs.ThisDog.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class
+            clsDogCollection AllDogs = new clsDogCollection();
+            //create the item of test data
+            clsDog TestItem = new clsDog();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.Age = 2;
+            TestItem.Breed = "Bulldog";
+            TestItem.Gender = "Male";
+            TestItem.Name = "Rex";
+            TestItem.Size = "Small";
+            TestItem.DateAdded = DateTime.Now.Date;
+            //set Thisdog to the test data;
+            AllDogs.ThisDog = TestItem;
+            //add the record
+            PrimaryKey = AllDogs.Add();
+            //set the primary key of the test data
+            TestItem.DogID = PrimaryKey;
+            //modify the record
+            TestItem.Age = 7;
+            TestItem.Breed = "Mixed Breed";
+            TestItem.Gender = "Female";
+            TestItem.Name = "Lilly";
+            TestItem.Size = "Extra Large";
+            TestItem.DateAdded = DateTime.Now.Date;
+            //set the record based on new data
+            AllDogs.ThisDog = TestItem;
+            //update the record
+            AllDogs.Update();
+            //find the record
+            AllDogs.ThisDog.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.AreEqual(AllDogs.ThisDog, TestItem);
         }
     }
 }
